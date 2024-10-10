b2f8b05b1b68a2290b40d5952a9f28718911254d
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2024-10-10 19:49:06 +0300

    Update copy-constructor.cpp

--- copy-constructor/copy-constructor.cpp
+++ copy-constructor/copy-constructor.cpp
@@ -1,7 +1,33 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
+#include "copy-constructor.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+class Example {
+    int a;
+public:
+    void SetA(int value) { a = value; }
+    int GetA() const  { return a; }
+    Example() : a(0) {};
+    Example(const Example& e) : a(e.GetA()) {};
+    ~Example() {};
+
+    string GetString() { return "a = " + to_string(a) + "\n"; }
+};
+
+Example check(Example e) {
+    cout << e.GetString();
+    return e;
 }
 
+int main() {
+
+    Example e = Example();
+    Example b{ e };
+    Example d{ Example() };
+    Example c = check(e);
+    cout << c.GetString();
+
+}
