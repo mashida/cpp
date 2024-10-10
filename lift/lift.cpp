e3afd375eca3981403a775dd7f11bbef5206c5e3
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2024-10-10 18:20:58 +0300

    added money example

--- lift/lift.cpp
+++ lift/lift.cpp
@@ -29,22 +29,40 @@ private:
 
 };
 
+class Money
+{
+	float mAmount;
+public:
+	// getter & setter
+	void SetMoney(float value) { mAmount = value; }
+	float GetMoney() { return mAmount;  }
+	// ctor & dtor
+	Money(float a) : mAmount(a) {};
+	~Money() {};
+	// output
+	void Print() { cout << "We have " << mAmount << " rubles.\n"; }
+private:
+
+};
+
+
 int main()
 {
 	setlocale(LC_ALL, "");
 
-	// just a comment
-	CLift lift = CLift(1, 9);
-	lift.Print();
+	Money money = Money(100);
+	money.Print(); // 100
+
+	cout << "Enter you money amount, please.\n";
+	float temp = 0;
+	cin >> temp;
+	money.SetMoney(temp);
+
+	// проверяем если нет денег
+	if (money.GetMoney() == 0)
+	{
+		cout << "We have no money.";
+	}
+
+	money.Print(); // 0
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
