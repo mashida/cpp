e082a56813f3ca58e8ac9ba2512445d19bf225dc
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2024-10-12 12:26:07 +0300

    Update copy-constructor.cpp

--- copy-constructor/copy-constructor.cpp
+++ copy-constructor/copy-constructor.cpp
@@ -1,20 +1,20 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
-#include "copy-constructor.h"
 
 using namespace std;
 
 class Example {
-    int a;
+    int *a;
 public:
-    void SetA(int value) { a = value; }
-    int GetA() const  { return a; }
-    Example() : a(0) {};
-    Example(const Example& e) : a(e.GetA()) {};
-    ~Example() {};
+    void SetA(int value) { a = &value; }
+    int* GetA() const  { return a; }
+    Example() : a(new int(0)) {};
+    Example(const Example& e) { a = new int(*e.GetA()); };
+    ~Example() { delete a; };
 
-    string GetString() { return "a = " + to_string(a) + "\n"; }
+    string GetString() { return "a = " + to_string(*a) + "\n"; }
+    void GetPointerOfA() { cout << "a: " << a << "\n"; }
 };
 
 Example check(Example e) {
@@ -27,7 +27,8 @@ int main() {
     Example e = Example();
     Example b{ e };
     Example d{ Example() };
-    Example c = check(e);
-    cout << c.GetString();
+    e.GetPointerOfA();
+    b.GetPointerOfA();
+    d.GetPointerOfA();
 
 }
