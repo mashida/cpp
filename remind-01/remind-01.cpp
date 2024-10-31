be7f7d0315883f994a452482a8ba33cd530f268f
Nikita Serditov <34740100+mashida@users.noreply.github.com>
2024-10-31 18:26:00 +0300

    Update remind-01.cpp
    Added class polymorphism example

--- remind-01/remind-01.cpp
+++ remind-01/remind-01.cpp
@@ -1,11 +1,18 @@
 ﻿#include <iostream>
+class Example {
+	int value;
+public:
+	int Value() { return value; }
+	void Value(int digit) { value = digit; }
+};
+
 
 void Print(int value)
 {
 	std::cout << value << "\n";
 }
 
-void Print(double value) 
+void Print(double value)
 {
 	std::cout << value << "\n";
 }
@@ -16,4 +23,8 @@ int main()
 	int intValue = 5;
 	Print(doubleValue);
 	Print(intValue);
+
+	Example example;
+	example.Value(16);
+	std::cout << example.Value();
 }
